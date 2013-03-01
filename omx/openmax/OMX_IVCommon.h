6c911dfafbac0dcbf76b022c8441f29a038b2fef
Mohammed Hassan <mohammed.hassan@jollamobile.com>
2013-03-01 17:32:21 +0200

    YUMB

--- omx/openmax/OMX_IVCommon.h
+++ omx/openmax/OMX_IVCommon.h
@@ -133,7 +133,7 @@ typedef enum OMX_COLOR_FORMATTYPE {
     OMX_COLOR_FormatKhronosExtensions = 0x6F000000, /**< Reserved region for introducing Khronos Standard Extensions */ 
     OMX_COLOR_FormatVendorStartUnused = 0x7F000000, /**< Reserved region for introducing Vendor Extensions */
     OMX_COLOR_FormatMax = 0x7FFFFFFF,
-    OMX_COLOR_FormatYUV420MBPackedSemiPlanar = (0x7FFFFFFF-1)
+    OMX_COLOR_FormatYUV420MBPackedSemiPlanar = 0x7FFFFFFE
 } OMX_COLOR_FORMATTYPE;
 
 
