b6412781bd50ae0ce5dde1ffc2165584ba62a9fe
Mohammed Hassan <mohammed.hassan@jollamobile.com>
2013-03-08 13:06:19 +0200

    If the sink doesn't specify a video format then force YUMB

--- omx/gstomxvideodec.c
+++ omx/gstomxvideodec.c
@@ -1045,8 +1045,8 @@ gst_omx_video_dec_negotiate (GstOMXVideoDec * self)
   if (!gst_structure_get_fourcc (s, "format", &fourcc) ||
       (format =
           gst_video_format_from_fourcc (fourcc)) == GST_VIDEO_FORMAT_UNKNOWN) {
-    GST_ERROR_OBJECT (self, "Invalid caps: %" GST_PTR_FORMAT, intersection);
-    return FALSE;
+    GST_WARNING_OBJECT (self, "Caps doesn't specify a format. Forcing YUMB");
+    format = GST_VIDEO_FORMAT_YUMB;
   }
 
   switch (format) {
